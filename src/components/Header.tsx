957aee89df2917b35d81a6e34d50e343cae59e38
YERSON PEREZ <tienwatch@gmail.com>
2026-05-13 18:27:56 -0500

    Delete src/components directory

--- src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Header: React.FC = () => (
-  <header>
-    <h1>👑 GANADERO ÉLITE</h1>
-    <p className="subtitle">Gestión Inteligente</p>
-  </header>
-)
--- src/components/Toast.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Toast: React.FC<{ mensaje: string }> = ({ mensaje }) => (
-  <div className="toast" dangerouslySetInnerHTML={{ __html: mensaje }} />
-)
-
-export default Toast
